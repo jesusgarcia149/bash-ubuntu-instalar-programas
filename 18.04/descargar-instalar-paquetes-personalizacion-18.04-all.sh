80990e9a3c24268fb4af43e5fecd9b2119cb346d
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2020-10-10 18:24:45 -0400

    Update descargar-instalar-paquetes-personalizacion-18.04-all.sh

--- 18.04/descargar-instalar-paquetes-personalizacion-18.04-all.sh
+++ 18.04/descargar-instalar-paquetes-personalizacion-18.04-all.sh
@@ -2,6 +2,11 @@
 
 #zorin-theme
 #url: https://launchpad.net/%7Ezorinos/+archive/ubuntu/stable/+index?batch=75&direction=backwards&memo=150&start=75
-sudo add-apt-repository ppa:zorinos/stable -y
+#sudo add-apt-repository ppa:zorinos/stable -y
+#sudo apt update
+#sudo apt install xfce4-zorinmenulite-plugin zorin-agent-geoclue2 zorin-desktop-themes zorin-os-default-settings zorin-os-wallpapers zorin-appearance zorin-exec-guard zorin-os-file-templates zorin-os-wallpapers-15 zorin-appearance-layouts-support zorin-exec-guard-app-db zorin-os-lite-core zorin-sound-theme zorin-appearance-layouts-xfce-core zorin-icon-themes zorin-os-lite-default-settings zorin-auto-theme zorin-os-artwork zorin-os-lite-desktop thunar -y
+
+#flat-remix-theme
+sudo add-apt-repository ppa:daniruiz/flat-remix
 sudo apt update
-sudo apt install xfce4-zorinmenulite-plugin zorin-agent-geoclue2 zorin-desktop-themes zorin-os-default-settings zorin-os-wallpapers zorin-appearance zorin-exec-guard zorin-os-file-templates zorin-os-wallpapers-15 zorin-appearance-layouts-support zorin-exec-guard-app-db zorin-os-lite-core zorin-sound-theme zorin-appearance-layouts-xfce-core zorin-icon-themes zorin-os-lite-default-settings zorin-auto-theme zorin-os-artwork zorin-os-lite-desktop thunar -y
+sudo apt install flat-remix flat-remix-gtk
