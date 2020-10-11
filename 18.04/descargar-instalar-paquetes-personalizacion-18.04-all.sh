37aeda269d00ea98fee5bf51f9b65f11048ff6c0
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2020-10-10 20:47:57 -0400

    Update descargar-instalar-paquetes-personalizacion-18.04-all.sh

--- 18.04/descargar-instalar-paquetes-personalizacion-18.04-all.sh
+++ 18.04/descargar-instalar-paquetes-personalizacion-18.04-all.sh
@@ -7,9 +7,9 @@
 #sudo apt install xfce4-zorinmenulite-plugin zorin-agent-geoclue2 zorin-desktop-themes zorin-os-default-settings zorin-os-wallpapers zorin-appearance zorin-exec-guard zorin-os-file-templates zorin-os-wallpapers-15 zorin-appearance-layouts-support zorin-exec-guard-app-db zorin-os-lite-core zorin-sound-theme zorin-appearance-layouts-xfce-core zorin-icon-themes zorin-os-lite-default-settings zorin-auto-theme zorin-os-artwork zorin-os-lite-desktop thunar -y
 
 #arc-theme papirus-icons-theme
-sudo apt install arc-theme papirus-icon-theme
+sudo apt install arc-theme papirus-icon-theme -y
 
 #flat-remix-theme
-sudo add-apt-repository ppa:daniruiz/flat-remix
+sudo add-apt-repository ppa:daniruiz/flat-remix -y 
 sudo apt update
-sudo apt install flat-remix flat-remix-gtk
+sudo apt install flat-remix flat-remix-gtk -y
