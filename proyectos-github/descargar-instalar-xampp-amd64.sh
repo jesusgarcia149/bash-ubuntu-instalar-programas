07e5f4dea639a86fa1a83846da4edccbfaf62bac
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2020-10-20 12:05:57 -0400

    Add files via upload

--- proyectos-github/descargar-instalar-xampp-amd64.sh
+++ proyectos-github/descargar-instalar-xampp-amd64.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 instalamos_xampp() {
-	sudo apt install wget -y
-	sudo wget https://downloadsapachefriends.global.ssl.fastly.net/7.4.10/xampp-linux-x64-7.4.10-0-installer.run
+	#definimos la ubicacion del archivo
+	archivo=./xampp-linux-x64-7.4.10-0-installer.run
+	#si no existe el archivo lo descargamos
+	if [ ! -f "$archivo" ]; then
+		sudo apt install wget -y
+		sudo wget https://downloadsapachefriends.global.ssl.fastly.net/7.4.10/xampp-linux-x64-7.4.10-0-installer.run
+	fi
 	sudo chmod 777 ./xampp-linux-x64-7.4.10-0-installer.run
 	sudo ./xampp-linux-x64-7.4.10-0-installer.run
 }
--- proyectos-github/descargar-instalar-xampp-i386.sh
+++ proyectos-github/descargar-instalar-xampp-i386.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 instalamos_xampp() {
-	sudo apt install wget -y
-	sudo wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/5.5.35/xampp-linux-5.5.35-0-installer.run/download
+	#definimos la ubicacion del archivo
+	archivo=./xampp-linux-5.5.35-0-installer.run
+	#si no existe el archivo lo descargamos
+	if [ ! -f "$archivo" ]; then
+		sudo apt install wget -y
+		sudo wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/5.5.35/xampp-linux-5.5.35-0-installer.run/download
+	fi
 	sudo chmod 777 ./xampp-linux-5.5.35-0-installer.run
 	sudo ./xampp-linux-5.5.35-0-installer.run/download
 }
