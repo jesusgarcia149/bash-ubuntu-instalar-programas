00b1cd58d4a0e5aa253e974349f918712c61fb24
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2020-11-03 02:30:00 +0000

    Add files via upload

--- 16.04/descargar-almacenar-paquetes-emuladores-16.04-all.sh
+++ 16.04/descargar-almacenar-paquetes-emuladores-16.04-all.sh
@@ -18,56 +18,59 @@ if [ ! -f "$archivo" ]; then
 fi
 DIRECTORIO=$(cat $archivo)
 
-#creamos el directorio
+#15.-especializacion
+sudo mkdir $DIRECTORIO/15.-especializacion
+#1.-produccion-audiovisual
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro
 
 #nes
 sudo apt-get install --download-only nestopia -y
-sudo mkdir $DIRECTORIO/1.-nes
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/1.-nes
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/1.-nes
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/1.-nes
 sudo apt-get clean
 
 #sg
 sudo add-apt-repository ppa:neon1ks/bionic -y
 sudo apt-get update
 sudo apt-get install --download-only kega-fusion -y
-sudo mkdir $DIRECTORIO/3.-sg
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/3.-sg
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/3.-sg
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/3.-sg
 sudo apt-get clean
 
 #snes
 sudo apt-get install --download-only zsnes -y
-sudo mkdir $DIRECTORIO/2.-snes
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/2.-snes
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/2.-snes
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/2.-snes
 sudo apt-get clean
 
 #gba
 sudo apt-get install --download-only mednafen -y
-sudo mkdir $DIRECTORIO/4.-gba
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/4.-gba
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/4.-gba
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/4.-gba
 sudo apt-get clean
 
 #n64
 sudo apt-get install --download-only mupen64plus-qt -y
-sudo mkdir $DIRECTORIO/5.-n64
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/5.-n64
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/5.-n64
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/5.-n64
 sudo apt-get clean
 
 #nds
 sudo apt-get install --download-only desmume -y
-sudo mkdir $DIRECTORIO/6.-nds
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/6.-nds
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/6.-nds
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/6.-nds
 sudo apt-get clean
 
 #gc
 sudo apt-get install --download-only dolphin-emu -y
-sudo mkdir $DIRECTORIO/7.-gc
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/7.-gc
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/7.-gc
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/7.-gc
 sudo apt-get clean
 
 #psp
 sudo add-apt-repository ppa:ppsspp/stable -y
 sudo apt update
 sudo apt-get install --download-only ppsspp -y
-sudo mkdir $DIRECTORIO/8.-psp
-sudo cp /var/cache/apt/archives/* $DIRECTORIO/8.-psp
+sudo mkdir $DIRECTORIO/15.-especializacion/3.-consolas-retro/8.-psp
+sudo cp /var/cache/apt/archives/* $DIRECTORIO/15.-especializacion/3.-consolas-retro/8.-psp
 sudo apt-get clean
