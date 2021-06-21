9ca0c9c8026bfa020fd0bdc4a34c281214d4dacf
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2021-06-21 11:15:45 -0400

    Add files via upload

--- 20.04/descargar-instalar-paquetes-personalizacion-20.04-all.sh
+++ 20.04/descargar-instalar-paquetes-personalizacion-20.04-all.sh
@@ -34,3 +34,27 @@
 #sudo add-apt-repository ppa:communitheme/ppa -y
 #sudo apt update
 #sudo apt install yaru-theme-gnome-shell yaru-theme-icon yaru-theme-unity yaru-theme-gtk yaru-theme-sound -y
+
+#wallpapers
+#sudo apt install ubuntu-wallpapers-bionic -y
+
+#Huawei Theme
+sudo apt install wget -y
+#creamos las carpetas
+sudo mkdir /home/jesus/.icons
+sudo mkdir /home/jesus/.themes
+#descargamos de instalamos los iconos
+# Glory icon theme(Huawei Icons)
+#https://www.xfce-look.org/s/XFCE/p/1341835
+#https://github.com/yeyushengfan258/Glory-icon-theme
+sudo wget https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MTk5Njc1ODQiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjMwYzI4ZTFjZTVjZGJmNDU3NjNhNGQwOTA4YjViY2E0OWRmMmNiNjU5ZmM1YTBiMTc3ZWZjYjhhMzgxMjkyMDNlNWVkOTgxMzdjYjNhM2U5MmZhZDIxZDBiNzMzMGQ3MmQ0YTliMjYwYWZjYzRjMGRjMTE1NGFkOWUxOGQ0MzNhIiwidCI6MTYyMzg1OTkzOCwic3RmcCI6IjdhOWZlMWJkMTM3YzZmNjRlZTZlYzc5MjdlMzIyOTEyIiwic3RpcCI6IjE4Ni45My4xOTAuMTAifQ.g85VslJu5WbCtjVlME427fptbdkx1LSkVJlnm60CRvQ/Glory.tar.xz
+sudo tar xvf Glory.tar.xz
+sudo mv Glory Glory-dark /home/jesus/.icons
+#descargamos de instalamos los temas
+# Windows 10 gtk theme (Huawei GTK)
+#https://www.xfce-look.org/p/1216281/
+sudo wget https://dllb.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE1MTkzOTQxNzEiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjY4NGQ1ZjI1NDNjMDVmZmZhNGM4Nzc5ZTJlOWJjNDhlNGIzZWIzZTkxOWU5ZDhjMWNiNmI3ZjJlMDBiZGY1YmZlYTYwMzc3NTY0MjE0ZGQwYjlkN2RlYzY0MzZmMGY5OTc5NTMwNGQ2MjhlZjNlNjMyMDk0NDhjY2FmMjQ0YjNkIiwidCI6MTYyMzg1ODYzNSwic3RmcCI6bnVsbCwic3RpcCI6bnVsbH0.7aB9xcgUNdkBluXmvmKDCgsaFUX9R9eJHUrKTDSUzHc/windows-10-master.zip
+sudo unzip windows-10-master.zip
+sudo mv Windows-10-master /home/jesus/.themes
+#localizacion de los wallpapers
+https://drive.google.com/drive/folders/1anCThE5X0LWaOPdr3nCrUVl38PC0cYB3
