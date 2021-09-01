906fb3d16ac2fc1a4ffed78135861e7c2e6b40db
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2021-09-01 16:58:02 -0400

    Add files via upload

---
+++ 18.04/descargar-instalar-paquetes-compatibilidad-18.04-all.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+#cambiar al directorio home
+cd
+
+#actualizar repositorios
+sudo apt update
+
+#wine
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+sudo apt update
+sudo apt install --install-recommends winehq-stable -y
+
+#winetricks
+sudo apt install winetricks -y
+
+#playonlinux
+sudo apt install playonlinux -y
+
+#lutris
+sudo add-apt-repository ppa:lutris-team/lutris -y
+sudo apt update
+sudo apt install lutris -y
+
+#steam
+sudo apt install steam -y
+
+
+
+
+
+
