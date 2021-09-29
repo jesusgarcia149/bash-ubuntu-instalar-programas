cc6f52b3b599a5c579c7f0470eb1cebf57455da0
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2021-09-28 21:39:20 -0400

    Update descargar-instalar-paquetes-basicos-18.04-amd64.sh

--- 18.04/descargar-instalar-paquetes-basicos-18.04-amd64.sh
+++ 18.04/descargar-instalar-paquetes-basicos-18.04-amd64.sh
@@ -12,7 +12,8 @@ sudo apt install wget -y
 #navegadores
 #firefox
 sudo apt install firefox -y
-#chromewget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt update
 sudo apt install google-chrome-stable -y
